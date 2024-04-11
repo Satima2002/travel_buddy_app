c3fbd3046698ac1d2b9ae33af04f74211c10d6d5
Satima Valsony <valsonysatima2002@gmail.com>
2024-04-11 15:07:13 +0200

    initial project

---
+++ src/test/java/com/example/travel_buddy_app/TravelBuddyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travel_buddy_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBuddyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
