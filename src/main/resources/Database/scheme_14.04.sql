d6b2e81e208dbc85f802441af1e2575c9723d688
Anna Shkaeva <anna.shkajeva@gmail.com>
2024-04-14 14:41:39 +0300

    Updated Blog entity 'season_visited' attribute data type, from date to string.

---
+++ src/main/resources/Database/scheme_14.04.sql
@@ -0,0 +1,54 @@
+-- create database travel_web_app
+create table users (
+id serial Primary Key,
+first_name varchar(50) not null,
+last_name varchar(50) not null,
+dob DATE not null,
+email varchar(50) not null,
+password varchar(50) not null,
+profile_photo JSONB not null,
+description varchar(500) null
+);
+
+--drop table users;
+--drop table trip;
+--drop sequence trip_id_seq;
+--drop table blog;
+--drop table host;
+
+create table trip (
+id serial Primary Key,
+destination_country varchar(50) not null,
+destination_city varchar(50) not null,
+start_date date not null,
+end_date date not null,
+budget money not null,
+type_name varchar(50) not null,
+transport_name varchar(50) not null,
+description varchar(500) null,
+user_id int not null,
+FOREIGN KEY (user_id) references users(id) on delete cascade
+);
+
+
+create table blog (
+id serial Primary Key,
+title varchar(50) not null,
+country varchar(50) not null,
+city varchar(50) not null,
+season_visited String not null,
+description varchar(500) null,
+user_id int not null,
+FOREIGN KEY (user_id) references users(id) on delete cascade
+);
+
+create table host (
+id serial Primary Key,
+country varchar (50) not null,
+city varchar (50) not null,
+available_start_date date not null,
+available_end_date date not null,
+house_type varchar(50) null,
+user_id int not null,
+FOREIGN KEY (user_id) references users(id) on delete cascade
+);
