5a67cf4590b60c19082990e29d656f796226764f
Satima Valsony <63864609+Satima2002@users.noreply.github.com>
2024-04-22 02:06:12 +0200

    Update BlogController.java
    controller adapted to thymleaf

--- src/main/java/com/example/travel_buddy_app/controllers/BlogController.java
+++ src/main/java/com/example/travel_buddy_app/controllers/BlogController.java
@@ -10,32 +10,39 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import com.example.travel_buddy_app.services.BlogService;
 import java.util.List;
 import java.util.List;
 
-@RestController
-@RequestMapping(path = "blogs")
+@Controller
+@RequestMapping
 public class BlogController {
 
     @Autowired
     private final BlogService blogService;
 
+
     public BlogController(BlogService blogService) {
         this.blogService = blogService;
     }
 
-    @GetMapping
-    public List<Blog> findAllBlogs() {
-        return blogService.findAllBlogs();
+    @GetMapping("/blogs")
+    public String findAllBlogs(Model model) {
+        List<Blog> allBlogs = blogService.findAllBlogs();
+        // Add items to model attribute
+        model.addAttribute("blogs", allBlogs);
+
+        return "blogs"; // Return Thymeleaf template name
     }
-    @GetMapping("/{id}")
+    @GetMapping("/blog/{id}")
     public Blog getBlogById(@PathVariable("id") long id) {
         return blogService.getBlogById(id);
     }
 
-    @PostMapping("/add-blog")
+    @PostMapping("/blog/add-blog")
     public ResponseEntity<?> addNewBlog(@RequestBody Blog blog) {
         try {
             blogService.validatePostBlog(blog);
@@ -46,12 +53,45 @@ public class BlogController {
         }
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/blog/{id}")
     public void deleteBlogById(@PathVariable("id") Long id) {
         blogService.deleteBlogById(id);
     }
 
+<<<<<<< Thymeleaf-trips-dashboard
+    @PutMapping("/blog/{id}/title")
+    public ResponseEntity<Blog> updateTitle(@PathVariable Long id, @RequestBody Blog newTitle) {
+        try {
+            blogService.updateTitle(id, newTitle);
+            return ResponseEntity.ok(newTitle);
+        } catch (IllegalArgumentException ex) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PutMapping("/blog/{id}/description")
+    public ResponseEntity<Blog> updateDescription(@PathVariable Long id, @RequestBody Blog newDescription) {
+        try {
+            blogService.updateTitle(id, newDescription);
+            return ResponseEntity.ok(newDescription);
+        } catch (IllegalArgumentException ex) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/search-blog")
+    public ResponseEntity<List<Blog>> searchBlogs(@RequestParam String searchText) {
+        List<Blog> foundTrips = blogService.searchBlog(searchText);
+        if (!foundTrips.isEmpty()) {
+            return ResponseEntity.ok(foundTrips);
+        } else {
+            return ResponseEntity.noContent().build();
+        }
+    }
+    @PutMapping("/blog/{id}")
+=======
     @PutMapping("/{id}")
+>>>>>>> main
     public Blog updateBlog(@PathVariable Long id, @RequestBody Blog blog) {
         return blogService.updateBlog(id, blog);
     }
